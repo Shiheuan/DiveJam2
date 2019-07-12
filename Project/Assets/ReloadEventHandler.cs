eba8c2eea5b77042d3314405783287578c39ec61
Shiheuan <shiheuan@gmail.com>
2019-07-13 00:16:12 +0800

    first commit basic functionality.

---
+++ Project/Assets/ReloadEventHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ReloadEventHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public float threshold = 80f;
+    private bool isPress = false;
+    private bool isReloaded = false;
+
+    private Vector3 clickPoint;
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPress && !isReloaded)
+        {
+            var end = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(end, clickPoint) > threshold)
+            {
+                isReloaded = true;
+                // todo: reload arrows.
+                print("reloading!!");
+            }
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPress = true;
+        clickPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPress = false;
+        isReloaded = false;
+    }
+}
---
+++ Project/Assets/Scripts/GirlEventHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.EventSystems;
+
+public class GirlEventHandler : MonoBehaviour, IPointerDownHandler
+{
+    private bool isDone = false;
+
+    public int pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(WaitSeconds());
+    }
+
+    IEnumerator WaitSeconds()
+    {
+        // todo: girl can't wait animation.
+        yield return new WaitForSeconds(3f);
+
+        if (!isDone)
+        {
+            // todo: girl "heng" animation and gameover.
+            print("heng");
+            Destroy(this);
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        print("game over!");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print("shot this girl.");
+        isDone = true;
+        // todo: move this to objects pool.
+        
+        this.gameObject.SetActive(false);
+        GameManager.Instance().Remove(pos);
+    }
+}
